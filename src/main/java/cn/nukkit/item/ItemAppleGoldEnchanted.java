e973afbc8f7c0fadecb1193bbf4a73ecfa8892f2
AllMcpeYT <uberdude123098@gmail.com>
2016-07-21 12:23:13 -0500

    Update ItemAppleGoldEnchanted.java

--- src/main/java/cn/nukkit/item/ItemAppleGoldEnchanted.java
+++ src/main/java/cn/nukkit/item/ItemAppleGoldEnchanted.java
@@ -1,9 +1,5 @@
 package cn.nukkit.item;
 
-/**
- * Created by Snake1999 on 2016/1/14.
- * Package cn.nukkit.item in project nukkit.
- */
 public class ItemAppleGoldEnchanted extends ItemEdible {
     public ItemAppleGoldEnchanted() {
         this(0, 1);
@@ -14,6 +10,6 @@ public class ItemAppleGoldEnchanted extends ItemEdible {
     }
 
     public ItemAppleGoldEnchanted(Integer meta, int count) {
-        super(GOLDEN_APPLE_ENCHANTED, meta, count, TextFormat.BLUE + "Enchanted Golden Apple");
+        super(GOLDEN_APPLE_ENCHANTED, meta, count, TextFormat.BLUE + "Enchanted" + TextFormat.YELLOW + "Golden Apple");
     }
 }
