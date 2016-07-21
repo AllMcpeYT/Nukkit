870bd9a09bbcd6429f5a3825ea65f17d51866a1a
AllMcpeYT <uberdude123098@gmail.com>
2016-07-21 17:53:16 -0500

    Update ItemDoorAcacia.java

--- src/main/java/cn/nukkit/item/ItemDoorAcacia.java
+++ src/main/java/cn/nukkit/item/ItemDoorAcacia.java
@@ -12,7 +12,7 @@ public class ItemDoorAcacia extends Item {
     }
 
     public ItemDoorAcacia(Integer meta, int count) {
-        super(ACACIA_DOOR, 0, count, "Acacia Door");
+        super(ACACIA_DOOR, 0, count, TextFormat.GOLD + "Acacia Door");
         this.block = new BlockDoorAcacia();
     }
 
