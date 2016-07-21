fcbf47e0a9e28849656ffb1efd80d113c16b62f9
AllMcpeYT <uberdude123098@gmail.com>
2016-07-20 19:02:55 -0500

    Create BroadcastCommand.java

---
+++ src/main/java/cn/nukkit/command/defaults/BroadcastCommand.java
@@ -0,0 +1,10 @@
+package cn.nukkit.command.defaults;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.TranslationContainer;
+
+import java.util.Objects;
+
+public class BroadcastCommand extends VanillaCommand {
