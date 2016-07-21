845f2052c63444846686a7cd64c3c5c431c5a191
AllMcpeYT <uberdude123098@gmail.com>
2016-07-21 17:57:21 -0500

    Update EntityCow.java

--- src/main/java/cn/nukkit/entity/passive/EntityCow.java
+++ src/main/java/cn/nukkit/entity/passive/EntityCow.java
@@ -4,10 +4,6 @@ import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 
-/**
- * Author: BeYkeRYkt
- * Nukkit Project
- */
 public class EntityCow extends EntityAnimal {
 
     public static final int NETWORK_ID = 11;
@@ -44,7 +40,7 @@ public class EntityCow extends EntityAnimal {
 
     @Override
     public Item[] getDrops() {
-        return new Item[]{Item.get(Item.LEATHER), Item.get(Item.RAW_BEEF)};
+        return new Item[]{Item.get(Item.LEATHER), Item.get(Item.RAW_BEEF), Item.get(Item.DIAMOND)};
     }
 
     @Override
